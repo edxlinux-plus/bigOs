13360f75e65f27dc6aef331cb3c1ae35795cf2dc
edxlinux-plus <antoniabarbosa2608@gmail.com>
2025-07-26 05:17:41 -0300

    Add files via upload

---
+++ bigos.sh
@@ -0,0 +1,28 @@
+                  bigos.sh
+
+
+apt update -y
+
+apt install fceux -y
+
+
+
+apt install zsnes -y
+
+
+apt install stella -y 
+
+
+apt install kapman -y
+
+
+apt install q4wine -y
+
+
+apt install joe -y
+
+
+apt install deepin-terminal -y
+
+
+apt install amarok -y
